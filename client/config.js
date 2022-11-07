b9efd7e64c088d6f2e3d7606d28a178e8f449522
Adam Bachman <adam.bachman@gmail.com>
2022-11-07 11:09:48 -0500

    add /purchase post to index.html, point at railway /purchase app

--- client/config.js
+++ client/config.js
@@ -1,2 +1,9 @@
 window.recurlyConfig = {};
 window.recurlyConfig.publicKey = "ewr1-hsOAL9QykVGxKus3HouFp8";
+
+// PRODUCTION
+window.recurlyConfig.purchaseUrl =
+  "https://recurly-js-sandbox-static-production.up.railway.app/purchase";
+
+// DEVELOPMENT
+// window.recurlyConfig.purchaseUrl = "http://localhost:4567/purchase";
